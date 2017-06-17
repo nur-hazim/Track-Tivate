b21318201eec3a3940e39e61fd1453038486b2f8
Commiter <nurhazim@yahoo.com.sg>
2017-06-18 07:44:28 +0800

    Trace Tivate
    Simplifies shopping and management in malls

---
+++ Track Tivate/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Track_Tivate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Payment()
+        {
+            return View();
+        }
+
+        public ActionResult Explore()
+        {
+            return View();
+        }
+    }
+}
---
+++ Track Tivate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Track_Tivate.Startup))]
+namespace Track_Tivate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
